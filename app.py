0a4774a5dcf3a7c658506c112f6ec62560bb698e
ghofranekhadhar <ghofrane.khadhar@gmail.com>
2026-04-19 04:00:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ class Cfg:
     FONT_R=FONT_DIR+"DejaVuSans.ttf"
     VF="fr-FR-DeniseNeural"; VG="fr-FR-HenriNeural"
     VRATE="-20%"; VPITCH="+5Hz"
-    MODEL="llama-3.1-8b-instant"
+    MODEL="llama3-8b-8192"
 
 # ─────────────────────────────────────────
 #  DATACLASSES
